7f75a95931055edcd6c061d3e0c1db57acc063d5
Ubuntu <ubuntu@ip-172-31-35-227.us-west-2.compute.internal>
2014-11-06 09:05:46 +0000

    fixed syntax

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.use(express.static(__dirname + '/public'))
 app.get('/', function(request, response) {
   var text = fs.readFileSync(index.html);
   response.send(text);
-}
+})
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
 })
