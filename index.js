378638769592f432a987d57faba87c24c8bd121c
Ubuntu <ubuntu@ip-172-31-35-227.us-west-2.compute.internal>
2014-11-06 09:04:31 +0000

    reading from HTML

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!')
-})
-
+  var text = fs.readFileSync(index.html);
+  response.send(text);
+}
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
 })
